e4aae09442620eaa35f312203cd5fed00a336b8e
fukayatsu <fukayatsu@gmail.com>
2013-11-09 23:40:37 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiniKanban::Application.config.secret_key_base = '01a091e325f38f0c4230a17d47a5c9381734bb108c8c7f107978b98574af14c07295ddfa49dc38f7c3f1f1ad00cd45955045261ce36e4f5d40e74b6a28289f99'
